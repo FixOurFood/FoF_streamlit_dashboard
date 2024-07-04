bb17d2280b744c38368ab9acc4e7aa82b32d05cf
Juan Pablo Cordero <34517350+jucordero@users.noreply.github.com>
2024-07-04 12:46:18 +0100

    Update datablock_setup.py

--- datablock_setup.py
+++ datablock_setup.py
@@ -144,7 +144,7 @@ pop_world_past = pop.sel(Year=np.arange(1961,2021), Region=area_pop_world)
 # Land use data
 # -------------------------------
 
-LC = UKCEH_LC_1000["percentage_aggregate"]
+LC = UKCEH_LC_1000_gb["percentage_aggregate"]
 
 datablock["land"]["percentage_land_use"] = LC.where(np.isfinite(ALC.grade))
 datablock["land"]["dominant_classification"] = ALC.grade
