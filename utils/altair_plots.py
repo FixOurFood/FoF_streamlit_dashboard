4f598168ec51ddbfff2b6f92b333d7429fc32b40
jucordero <obejaamarilla@hotmail.com>
2024-10-24 19:54:04 +0100

    Advanced settings, summary, new plots

--- utils/altair_plots.py
+++ utils/altair_plots.py
@@ -5,6 +5,10 @@ import pandas as pd
 import streamlit as st
 from glossary import *
 
+import base64
+from io import BytesIO
+from PIL import Image
+
 def plot_years_altair(food, show="Item", ylabel=None, colors=None, ymin=None, ymax=None):
 
     # If no years are found in the dimensions, raise an exception
@@ -120,29 +124,113 @@ def plot_land_altair(land):
 
     return c
 
-def plot_single_bar_altair(da, show="Item", x_axis_title=None, xmin=None, xmax=None, unit=""):
-    df = da.to_dataframe().reset_index().fillna(0)
-    df = df.melt(id_vars=show, value_vars=da.name)
+def plot_single_bar_altair(da, show="Item", axis_title=None,
+                                    ax_min=None, ax_max=None, unit="",
+                                    vertical=True, mark_total=False,
+                                    bar_width=80, show_zero=False,
+                                    ax_ticks=False, color=None):
+    
+    df_pos = da.where(da>0).to_dataframe().reset_index().fillna(0)
+    df_neg = da.where(da<0).to_dataframe().reset_index().fillna(0)
+
+    df_pos = df_pos.melt(id_vars=show, value_vars=da.name)
+    df_neg = df_neg.melt(id_vars=show, value_vars=da.name)
     
     # Create a new column for the tooltip with units
-    df['value_with_unit'] = df['value'].apply(lambda x: f"{x:.3f} {unit}")
+    df_pos['value_with_unit'] = df_pos['value'].apply(lambda x: f"{x:.2f} {unit}")
+    df_pos['order'] = np.arange(len(da[show].values))
+    df_neg['value_with_unit'] = df_neg['value'].apply(lambda x: f"{x:.2f} {unit}")
+    df_neg['order'] = np.arange(len(da[show].values))
 
     # Set yaxis limits
-    if xmax is None:
-        xmax = da.sum(dim=show).max().item()
-    if xmin is None:
-        xmin = np.min([da.sum(dim=show).min().item(), 0])
+    if ax_max is None:
+        ax_max = da.where(da>0).sum(dim=show).max().item()
+    if ax_min is None:
+        ax_min = np.min([da.where(da<0).sum(dim=show).min().item(), 0])
 
-    c = alt.Chart(df).mark_bar().encode(
-        x = alt.X('sum(value):Q',
-                  title=x_axis_title,
-                  axis=alt.Axis(labels=False),
-                  scale=alt.Scale(domain=(xmin, xmax))),
-        order=alt.Order(sort='descending'),
-        color=alt.Color(show, title=None, legend=None, scale=alt.Scale(scheme='category20b')),
+    if vertical:
+        chart_params = {"y":alt.Y('sum(value):Q',
+                            title=axis_title,
+                            axis=alt.Axis(labels=ax_ticks),
+                            scale=alt.Scale(domain=(ax_min, ax_max))),
+                        "x":alt.X('variable', axis=alt.Axis(labels=False, title=None))}
+        icon_params = {"y": "total", "x": "variable"}
+    else:
+        chart_params = {"x":alt.X('sum(value):Q',
+                            title=axis_title,
+                            axis=alt.Axis(labels=ax_ticks),
+                            scale=alt.Scale(domain=(ax_min, ax_max))),
+                        "y":alt.Y('variable', axis=alt.Axis(labels=False, title=None))}
+        icon_params = {"x": "total", "y": "variable"}
+
+    if color is None:
+        alt_color = alt.Color(show, title=None, legend=None, scale=alt.Scale(scheme='category20b'))
+    
+    else:
+        alt_color = alt.Color(show, title=None, legend=None,
+                          scale=alt.Scale(domain=color.keys(),
+                                          range=color.values()))
+
+    # Plot positive values
+    c = alt.Chart(df_pos).mark_bar().encode(
+        **chart_params,
+        color=alt_color,
         tooltip=[alt.Tooltip(f'{show}:N'),
                  alt.Tooltip('value_with_unit:N', title='Total')],
-    ).properties(height=80)
+        order=alt.Order(f'order:N', sort='ascending')
+    )
+
+    # Plot negative values
+    c += alt.Chart(df_neg).mark_bar().encode(
+        **chart_params,
+        color=alt_color,
+        tooltip=[alt.Tooltip(f'{show}:N'),
+                 alt.Tooltip('value_with_unit:N', title='Total')],
+        order=alt.Order(f'order:N', sort='ascending')
+    )
+    # Add a marker for the total
+    if mark_total == True:
+
+        img_path = "images/rhomboid.png"
+        pil_image = Image.open(img_path)
+        output = BytesIO()
+        pil_image.save(output, format='PNG')
+
+        base64_img = "data:image/png;base64," + base64.b64encode(output.getvalue()).decode()
+
+        total = da.sum(dim=show).item()
+        source = pd.DataFrame.from_records([
+            {"variable": da.name, "total": total, "total_with_unit": f"{total:.2f} {unit}",
+            "img": base64_img},
+        ])        
+
+        c += alt.Chart(source).mark_image(
+            width=25,
+            height=25
+        ).encode(
+            **icon_params,
+            url='img',
+            tooltip=[alt.Tooltip('total_with_unit:N', title='Total')]
+        )
+    
+    # Add a line for zero
+    if show_zero == True:
+        zero_line_params = {"y": "value"} if vertical else {"x": "value"}
+        c += alt.Chart(pd.DataFrame({
+            'value': 0.,
+            'color': ['black']
+            })).mark_rule(
+                color="black",
+                thickness=2,
+            ).encode(
+                **zero_line_params
+        )
+
+    # Set bar width
+    if vertical:
+        c = c.properties(width=bar_width)
+    else:
+        c = c.properties(height=bar_width)
 
     return c
 
