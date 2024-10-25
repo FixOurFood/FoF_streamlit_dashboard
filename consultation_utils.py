25171dd91b1085617831acf6aea13b5ebb26ef81
jucordero <obejaamarilla@hotmail.com>
2024-10-25 11:34:06 +0100

    Communicating with consultation spreadsheet

--- consultation_utils.py
+++ consultation_utils.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import gspread
 from google.oauth2 import service_account
+from utils.helper_functions import update_slider, reset_sliders
 
 SCOPES = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 SERVICE_ACCOUNT_FILE = dict(st.secrets["gspread"]["gs_api_key"])
@@ -10,9 +11,19 @@ credentials = service_account.Credentials.from_service_account_info(
 
 gc = gspread.authorize(credentials)
 sh = gc.open_by_key("1ZEb7PzEi6aKv303t7ypFriIt89FPzXTySGt_vmY60_Y")
-ws = sh.worksheet("Stage I submissions")
+stage_I_worksheet = sh.worksheet("Stage I submissions")
+pathways_worksheet = sh.worksheet("AFN Scenarios pass 1")
+enrolments_worksheet = sh.worksheet("Form responses 2")
 
 stage_I_deadline = 'December 31, 2024'
+
+def get_user_list():
+    """Get the list of user IDs from the Google Sheet"""
+
+    user_list = enrolments_worksheet.col_values(5)
+    user_list = user_list[1:]
+    return user_list
+
 
 def submit_scenario(user_id, ambition_levels=False):
     """Submit the pathway to the Google Sheet.
@@ -33,7 +44,7 @@ def submit_scenario(user_id, ambition_levels=False):
     """
     if not ambition_levels:
         row = [user_id, "test"]
-        ws.append_row(row)
+        stage_I_worksheet.append_row(row)
         return
     
     row = [user_id,
@@ -64,7 +75,71 @@ def submit_scenario(user_id, ambition_levels=False):
            st.session_state["DACCS"],
     ]
 
-    ws.append_row(row)
+    stage_I_worksheet.append_row(row)
+
+def get_pathways():
+    """Get the pathways names from the Google Sheet"""
+
+    values = pathways_worksheet.col_values(1)
+    return values[2:]
+
+def get_pathway_data(pathway_name):
+    """Get the scenario data from the Google Sheet"""
+
+    values = pathways_worksheet.col_values(1)
+    idx = values.index(pathway_name)
+
+    pathway_values = pathways_worksheet.row_values(idx + 1)
+    pathway_values = pathway_values[1:]
+
+    # Convert string values to numbers, replacing "no value" with 0
+    pathway_values = [float(x) if x != "Float" and x != "" else 0 for x in pathway_values]
+    
+    return pathway_values
+
+def call_scenarios():
+    """Call the scenarios from the Google Sheet"""
+        # reset all states
+    reset_sliders()
+    # get scenario state
+    scenario = st.session_state["scenario"]
+    pathway_data = get_pathway_data(scenario)
+
+    keys=[
+        "ruminant",
+        "dairy",
+        "pig_poultry_eggs",
+        "fruit_veg",
+        "cereals",
+        "waste",
+        "labmeat",
+
+        "pasture_sparing",
+        "arable_sparing",
+        "land_beccs",
+        "foresting_spared",
+        
+        "silvopasture",
+        "methane_inhibitor",
+        "manure_management",
+        "animal_breeding",
+        "fossil_livestock",
+        
+        "agroforestry",
+        "fossil_arable",
+        
+        "waste_BECCS",
+        "overseas_BECCS",
+        "DACCS",
+    ]
+
+    update_slider(keys, pathway_data)
+
 
 if __name__ == "__main__":
-    submit_scenario("TEST")
+    # submit_scenario("TEST")
+
+    pathway_names = get_pathways()
+    print(get_pathway_data(pathway_names[0]))
+
+    print(get_user_list())
