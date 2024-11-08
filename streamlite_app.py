4e16fc271c17c22c672d56fb72da4dc0624fb7bc
jucordero <obejaamarilla@hotmail.com>
2024-11-08 12:51:27 +0000

    default woodland sequestration

--- streamlite_app.py
+++ streamlite_app.py
@@ -198,8 +198,8 @@ with st.sidebar:
             max_ghge_animal = st.slider('Maximum animal production GHGE reduction due to innovation [%]', min_value=0, max_value=100, value=30, step=10, key = "max_ghg_animal", help = help["advanced_options"][3])
             max_ghge_plant = st.slider('Maximum plant production GHGE reduction due to innovation [%]', min_value=0, max_value=100, value=30, step=10, key = "max_ghg_plant", help = help["advanced_options"][4])
             bdleaf_conif_ratio = st.slider('Ratio of coniferous to broadleaved reforestation', min_value=0, max_value=100, value=50, step=10, key = "bdleaf_conif_ratio", help = help["advanced_options"][5])
-            bdleaf_seq_ha_yr = st.slider('Broadleaved forest CO2 sequestration [t CO2 / ha / year]', min_value=1., max_value=15., value=5., step=0.5, key = "bdleaf_seq_ha_yr", help = help["advanced_options"][6])
-            conif_seq_ha_yr = st.slider('Coniferous forest CO2 sequestration [t CO2 / ha / year]', min_value=1., max_value=30., value=5., step=0.5, key = "conif_seq_ha_yr", help = help["advanced_options"][7])
+            bdleaf_seq_ha_yr = st.slider('Broadleaved forest CO2 sequestration [t CO2 / ha / year]', min_value=1., max_value=15., value=12.5, step=0.5, key = "bdleaf_seq_ha_yr", help = help["advanced_options"][6])
+            conif_seq_ha_yr = st.slider('Coniferous forest CO2 sequestration [t CO2 / ha / year]', min_value=1., max_value=30., value=23.5, step=0.5, key = "conif_seq_ha_yr", help = help["advanced_options"][7])
             elasticity = st.slider("Production / Imports elasticity ratio", min_value=0., max_value=1., value=1., step=0.1, key="elasticity", help = help["advanced_options"][9])
             agroecology_tree_coverage = st.slider("Tree coverage in agroecology", min_value=0., max_value=1., value=0.1, step=0.1, key="tree_coverage")
             
@@ -255,8 +255,8 @@ with st.sidebar:
             max_ghge_plant = 30
             bdleaf_conif_ratio = 50
 
-            bdleaf_seq_ha_yr = 5.
-            conif_seq_ha_yr = 5.
+            bdleaf_seq_ha_yr = 12.5
+            conif_seq_ha_yr = 23.5
 
             elasticity = 1
             agroecology_tree_coverage = 0.1
